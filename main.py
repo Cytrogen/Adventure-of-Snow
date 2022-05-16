b43554a737ef611d7fe51ff1e1f6e991d6a6858b
Cytrogen <halloweendemon3418@gmail.com>
2022-05-15 23:31:55 -0400

    简化了主程序

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 # 游戏主程序
 
 
-from config import *
+from welcome import *
 
 
-welcomePath = os.path.join(TEXT_PATH, "welcome.json")
-
-
-title = json.load(open(welcomePath, 'r', encoding="UTF-8")).get('title')
-titlePost = random.choice(title)
-print(titlePost)
-
-hint = json.load(open(welcomePath, 'r', encoding="UTF-8")).get('hint')
-hintPost = random.choice(hint)
-print('\n' + hintPost)
+welcome()
